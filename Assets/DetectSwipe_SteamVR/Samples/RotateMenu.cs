01c1f4845a1f18850b85e7efc8f53bd9b8296c69
zabaglione <z.zabaglione@gmail.com>
2016-05-08 19:08:50 +0900

    初期リリース

---
+++ Assets/DetectSwipe_SteamVR/Samples/RotateMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using zabaglione.vive;
+
+public class RotateMenu : MonoBehaviour {
+	
+	[SerializeField] private float second=0.3f;
+	[SerializeField] GameObject target;
+	
+	private bool isRotate;
+	
+	void Start () {
+		ViveController vc = GetComponent<ViveController>();
+		vc.OnSwipe += OnSwipe;
+		isRotate = false;
+	}
+	
+	void OnSwipe(ViveController.SwipeDirection dir) {
+		if (isRotate == false) {
+			isRotate = true;
+			StartCoroutine(Rotation(dir));
+		}
+	}
+	
+	IEnumerator Rotation(ViveController.SwipeDirection dir) {
+		float angleDir = 0.0f;
+		switch(dir) {
+		case ViveController.SwipeDirection.LEFT:
+			angleDir = 1.0f;
+			break;
+		case ViveController.SwipeDirection.RIGHT:
+			angleDir = -1.0f;
+			break;
+		}
+		//Debug.Log("dir="+ dir+" angle="+angle);
+		
+		float angle = 0.0f;
+		while(angle < 90.0f){
+			float deltaAngle = 90 * Time.deltaTime * (1 / second);
+			target.transform.Rotate(new Vector3(0 ,0 , angleDir * deltaAngle));
+			angle += deltaAngle;
+			yield return null;
+		}
+		isRotate = false;
+		yield return null;
+	}
+}
---
+++ Assets/DetectSwipe_SteamVR/Scripts/ViveController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using UnityEngine;
+
+namespace zabaglione.vive {
+	
+	public class ViveController : MonoBehaviour {
+		
+		public enum SwipeDirection
+		{
+			NONE,
+			UP,
+			DOWN,
+			LEFT,
+			RIGHT
+		};
+		
+		
+		public event Action<SwipeDirection> OnSwipe;
+		public event Action<Vector2> OnClick;
+		public event Action<Vector2> OnDown;
+		public event Action<Vector2> OnUp;
+		public event Action<Vector2> OnDoubleClick;
+		
+		
+		[SerializeField] private float doubleClickTime = 0.3f; 
+		[SerializeField] private float swipeWidth = 0.5f;
+		
+		private Vector2 startPosition = Vector2.zero;
+		private Vector2 endPosition = Vector2.zero;
+		private Vector2 lastPosition = Vector2.zero;
+		
+		private float lastUpTime;
+		private float lastHorizontalValue;
+		private float lLastVerticalValue;
+		
+		public float DoubleClickTime{ get { return doubleClickTime; } }
+		
+		private SteamVR_TrackedObject trackedObject;
+		
+		void Start()
+		{
+			trackedObject = GetComponent<SteamVR_TrackedObject>();
+		}
+		
+		private void Update()
+		{
+			CheckInput();
+		}
+		
+		
+		private void CheckInput()
+		{
+			SwipeDirection swipe = SwipeDirection.NONE;
+			
+			var device = SteamVR_Controller.Input((int) trackedObject.index);
+			Vector2 position = device.GetAxis();
+			
+			if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad))
+			{
+				startPosition = position;
+				if (OnDown != null)
+					OnDown(position);
+			}
+			
+			if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
+			{
+				endPosition =lastPosition;
+				swipe = DetectSwipe ();
+			}
+			
+			if (swipe != SwipeDirection.NONE && OnSwipe != null)
+				OnSwipe(swipe);
+			
+			if(device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
+			{
+				if (OnUp != null)
+					OnUp(position);
+				
+				if (Time.time - lastUpTime < doubleClickTime)
+				{
+					if (OnDoubleClick != null)
+						OnDoubleClick(position);
+				}
+				else
+				{
+					if (OnClick != null)
+						OnClick(position);
+				}
+				
+				lastUpTime = Time.time;
+			}
+			lastPosition = position;
+		}
+		
+		
+		private SwipeDirection DetectSwipe ()
+		{
+			Vector2 swipeData = (endPosition - startPosition).normalized;
+			//Debug.Log("swipData="+swipeData);
+			bool swipeIsVertical = Mathf.Abs (swipeData.x) < swipeWidth;
+			bool swipeIsHorizontal = Mathf.Abs(swipeData.y) < swipeWidth;
+			
+			if (swipeData.y > 0f && swipeIsVertical)
+				return SwipeDirection.UP;
+			
+			if (swipeData.y < 0f && swipeIsVertical)
+				return SwipeDirection.DOWN;
+			
+			if (swipeData.x > 0f && swipeIsHorizontal)
+				return SwipeDirection.RIGHT;
+			
+			if (swipeData.x < 0f && swipeIsHorizontal)
+				return SwipeDirection.LEFT;
+			
+			return SwipeDirection.NONE;
+		}
+		
+		private void OnDestroy()
+		{
+	            // Ensure that all events are unsubscribed when this is destroyed.
+			OnSwipe = null;
+			OnClick = null;
+			OnDoubleClick = null;
+			OnDown = null;
+			OnUp = null;
+		}
+	}
+}
